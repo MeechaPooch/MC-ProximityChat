9f20fd06363b11216484873a7d4c3ee03f7b40da
meechapooch <powchm@POOCH2ELBOO.localdomain>
2021-03-18 12:50:02 -0700

    Create Blank Plugin

---
+++ src/main/java/com/mooo/pooch/proximitychatstandaside/Consts.java
@@ -0,0 +1,6 @@
+package com.mooo.pooch.proximitychatstandaside;
+
+public class Consts {
+    public static int SERVER_VOIP_PORT = 9720;
+    public static int CLIENT_VOIP_PORT = 9720;
+}
---
+++ src/main/java/com/mooo/pooch/proximitychatstandaside/ProximityChatStandaside.java
@@ -0,0 +1,17 @@
+package com.mooo.pooch.proximitychatstandaside;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProximityChatStandaside extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
